6b1cbc6ce5f82fcd3e59382e1e508779aa3402c2
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-05-13 16:33:27 -0400

    setup server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+const request = require('request')
+const morgan = require('morgan');
+const router = require('./routes.js')
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const localStrategy = require('passport-local').Strategy;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../client')));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+app.use(session({
+  secret:"someSecret",
+  resave: true,
+  saveUninitialized: true
+}));
+
+const port = process.env.PORT || 3005;
+app.listen(port,(err) => {
+  console.log("Listening on port " + port);
+});
