dfc22f73ba5aacd02dc43627f65bd777ac180de2
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-05-14 06:12:56 -0400

    remove dummy data

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let path = require('path');
 let WATSON = require('./watson');
-let dummyData = require('./dummyWatsonResponse');
 module.exports = (app) => {
   app.get('/', (req, res) => {
     // res.send(WATSON.analyzeText('A word is dead when it is said, some say. Emily Dickinson'))
