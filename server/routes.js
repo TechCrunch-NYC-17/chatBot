d2de0e3ea182d97065e07f788e29080d2a576c99
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-05-13 20:33:01 -0400

    Watson Whole Document analysis parser complete

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,10 @@
 let express = require('express');
 let path = require('path');
 let WATSON = require('./watson');
+let dummyData = require('./dummyWatsonResponse');
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    // WATSON.tone({ text: 'A word is dead when it is said, some say. Emily Dickinson' },
-    // function(err, tone) {
-    //   if (err){
-    //     console.log(err)
-    //     res.send(err);
-    //   }
-    //   else{
-    //     res.send(JSON.stringify(tone, null, 2));
-    //   }
-    // });
-    res.send('HOME')
+    // res.send(WATSON.analyzeText('A word is dead when it is said, some say. Emily Dickinson'))
+    res.send('Home');
   })
 }
--- server/watson.js
+++ server/watson.js
@@ -1,10 +1,42 @@
 const watson = require('watson-developer-cloud');
 
-let tone_analyzer = watson.tone_analyzer({
+const tone_analyzer = watson.tone_analyzer({
   username: '40f26cf0-4ac5-4db1-a72d-ecac530f9e83',
   password: 'c32JVLTp5Orh',
   version: 'v3',
   version_date: '2016-05-19'
 });
 
-module.exports = tone_analyzer;
+const watsonParserWholeDocument = (arr) => {
+  let obj = {};
+  arr.document_tone.tone_categories.forEach(a=>{
+    obj[a.category_name] = {};
+    a.tones.forEach(tone => {
+      obj[a.category_name][tone.tone_name] = tone.score
+    })
+  });
+  return obj
+};
+
+const analyzeText = (message) => {
+  if(Array.isArray(message)){
+    console.log(array);
+  } else {
+    tone_analyzer.tone({text: message}, (err, tone) => {
+      if (err){
+        console.log(err)
+        res.send(err);
+      }
+      else{
+        console.log(watsonParserWholeDocument(tone))
+        return watsonParserWholeDocument(tone);
+      }
+    })
+  }
+}
+
+
+module.exports = {
+  analyzeText,
+  // watsonParserWholeDocument
+};
